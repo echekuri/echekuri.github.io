9204b2454af16cbcfbe9ea00b8bce13c0c9b7709
Esha <echekuri@usc.edu>
2019-11-13 16:07:32 -0800

    Initial Commit

---
+++ Non-LinearNarrative/Interactive.js
@@ -0,0 +1,100 @@
+$(document).ready(function() {
+
+$("#button1").click(function() {
+  $("#class").css("color","black");
+  $("#classes").css("color","black");
+  $("#button1").hide();
+          });
+
+  $("#button2").click(function() {
+    $("#betaact1").css("color","black");
+    $("#button2").hide();
+          });
+
+  $("#button3").click(function() {
+      $("#gammaact1").css("color","black");
+      $("#button3").hide();
+          });
+
+  $("#button4").click(function() {
+      $("#deltaact1").css("color","black");
+      $("#button4").hide();
+          });
+
+  $("#button5").click(function() {
+      $("#epsilonact1").css("color","black");
+      $("#button5").hide();
+          });
+
+  $("#button6").click(function() {
+      $("#button7").css("color","black");
+      $("#button8").css("color","black");
+      $("#button9").css("color","black");
+      $("#button6").hide();
+          });
+
+  $("#button7").click(function(){
+      $("#button10").css("color","black");
+          });
+
+  $("#button8").click(function(){
+      $("#button11").css("color","black");
+          });
+
+  $("#button12").click(function() {
+      $("#button13").css("color","black");
+      $("#button14").css("color","black");
+      $("#button15").css("color","black");
+      $("#button12").hide();
+          });
+
+  $("#button13").click(function(){
+      $("#button16").css("color","black");
+          });
+
+  $("#button14").click(function(){
+      $("#button17").css("color","black");
+          });
+
+  $("#button18").click(function() {
+      $("#button19").css("color","black");
+      $("#button20").css("color","black");
+      $("#button21").css("color","black");
+      $("#button18").hide();
+          });
+
+  $("#button19").click(function(){
+      $("#button22").css("color","black");
+          });
+
+  $("#button20").click(function(){
+      $("#button23").css("color","black");
+          });
+
+  $("#button24").click(function() {
+      $("#alphaact1").css("color","black");
+      $("#button24").hide();
+          });
+
+  $("#button25").click(function() {
+      $("#alphaact2").css("color","black");
+      $("#button25").hide();
+      $("#button26").css("color","black");
+          });
+
+  $("#button26").click(function() {
+      $("#alphaact3").css("color","black");
+      $("#button26").hide();
+          });
+
+  $("#button27").click(function() {
+      $("#alphaact4").css("color","black");
+      $("#button27").hide();
+          });
+
+  $("#button28").click(function() {
+      $("#alphaact5").css("color","black");
+      $("#button28").hide();
+          });
+
+});
